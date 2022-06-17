1f078a2e77d43ba8c0177454c3864194333d98ae
guoyuncong <guoyuncong@lazbao.com>
2022-06-17 10:04:02 +0800

    feat: init

---
+++ src/main/java/yc/spring/configurationPrperties/Dog.java
@@ -0,0 +1,11 @@
+//package yc.spring.configurationPrperties;
+//
+///**
+// * @author: gyc
+// * @date: 2022/06/17 9:56
+// * @description:
+// */
+//public class Dog {
+//    private String name;
+//    private Integer age;
+//}
---
+++ src/main/java/yc/spring/configurationPrperties/Person.java
@@ -0,0 +1,25 @@
+//package yc.spring.configurationPrperties;
+//
+//import lombok.Data;
+//
+///**
+// * @author: gyc
+// * @date: 2022/06/17 9:56
+// * @description:
+// */
+//@Data
+//@Component
+//@ConfigurationProperties(prefix = "person")
+//public class Person {
+//    private String lastName;
+//    private Integer age;
+//    private Boolean boss;
+//    private Date birth;
+//    private Map<String, Object> maps;
+//    private Map<String, Dog> dogMaps;
+//    // private Object[] lists;
+//    private List<Object> lists;
+//    // private Dog[] dogs;
+//    private List<Dog> dogs;
+//    private Dog dog;
+//}
